5127ad2f447df0dae5592e48e90d4b7252366b51
MehmetCanWT <52177084+MehmetCanWT@users.noreply.github.com>
2022-08-02 17:06:23 +0300

    Update Hentai.js

--- Javascript/Hentai.js
+++ Javascript/Hentai.js
@@ -9,7 +9,7 @@ function fetchHentai() {
 
   let parentdiv = document.createElement("div");
   parentdiv.id = "memes";
-  fetch(`https://www.reddit.com/r/HENTAI_GIF.json?include_over_18=on&after=${after}`)
+  fetch(`https://www.reddit.com/r/hentai.json?include_over_18=on&after=${after}`)
     .then((response) => response.json())
     .then((body) => {
       after = body.data.after;
